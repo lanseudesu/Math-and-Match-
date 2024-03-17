8bb5bacd8fcafc6df7d0a163cd0da2ed44c1bdaf
lanseudesu <lantisfernandez29@gmail.com>
2024-03-18 04:09:06 +0800

    Finished adding buttons in home menu

--- lib/pages/main_menu.dart
+++ lib/pages/main_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Menu extends StatelessWidget {
   const Menu({super.key});
@@ -19,61 +20,194 @@ class Menu extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: 720, child: _gameTitle()),
-
-            // Add more widgets here if needed
-          ],
-        ),
+        child: _menuButtons(),
       ),
     ));
   }
 
+  Column _menuButtons() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        SizedBox(height: 350, child: _gameTitle()),
+        Column(children: [
+          SizedBox(height: 20),
+          SizedBox(
+            width: 200,
+            height: 80,
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    backgroundColor: Color(0xffFF52A4)),
+                child: Container(
+                  child: const Text(
+                    "PLAY",
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontFamily: 'Aero',
+                      color: Colors.white,
+                      shadows: [
+                        Shadow(
+                          offset: Offset(-2, -2), // Top left shadow
+                          color: Color(0xffFF00FF),
+                          blurRadius: 0,
+                        ),
+                        Shadow(
+                          offset: Offset(2, 2), // Bottom right shadow
+                          color: Color(0xff00FFFF),
+                          blurRadius: 0,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                onPressed: () {}),
+          ),
+          SizedBox(height: 10),
+          SizedBox(
+            width: 200,
+            height: 60,
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    backgroundColor: Color(0xffFF52A4)),
+                child: Container(
+                  child: const Text(
+                    "How to Play",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontFamily: 'Aero',
+                      color: Colors.white,
+                      shadows: [
+                        Shadow(
+                          offset: Offset(-1.5, -1.5), // Top left shadow
+                          color: Color(0xffFF00FF),
+                          blurRadius: 0,
+                        ),
+                        Shadow(
+                          offset: Offset(1.5, 1.5), // Bottom right shadow
+                          color: Color(0xff00FFFF),
+                          blurRadius: 0,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                onPressed: () {}),
+          ),
+          SizedBox(height: 10),
+          SizedBox(
+            width: 220,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: Color(0xffFF52A4),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: IconButton(
+                    alignment: Alignment.center,
+                    onPressed: () {},
+                    icon: SvgPicture.asset(
+                      'assets/icons/trophy-free-material-6-svgrepo-com.svg',
+                      height: 30,
+                      width: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: Color(0xffFF52A4),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: SvgPicture.asset(
+                      'assets/icons/settings.svg',
+                      height: 30,
+                      width: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: Color(0xffFF52A4),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: SvgPicture.asset(
+                      'assets/icons/exit.svg',
+                      height: 30,
+                      width: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ]),
+      ],
+    );
+  }
+
   Stack _gameTitle() {
     return Stack(
-      alignment: Alignment.topCenter,
+      alignment: Alignment.center,
       children: [
         Positioned(
-          top: 180,
+          bottom: 60,
           child: Text(
             'MATH',
             textAlign: TextAlign.center,
             style: TextStyle(
               fontSize: 80,
               fontWeight: FontWeight.w800,
-              fontFamily: 'Aero',
+              fontFamily: 'Archivo Black',
               letterSpacing: 2.0,
               foreground: Paint()
                 ..style = PaintingStyle.stroke
-                ..strokeWidth = 15
+                ..strokeWidth = 18
                 ..color = Color(0xffFF52A4),
             ),
           ),
         ),
         Positioned(
-          top: 180,
+          bottom: 60,
           child: Text(
             'MATH',
             textAlign: TextAlign.center,
             style: TextStyle(
               fontSize: 80,
               fontWeight: FontWeight.w800,
-              fontFamily: 'Aero',
+              fontFamily: 'Archivo Black',
               letterSpacing: 2.0,
               color: Colors.white,
             ),
           ),
         ),
         Positioned(
-          left: 78,
-          top: 267,
+          left: 52,
+          bottom: 12,
           child: Text(
             'and match',
             textAlign: TextAlign.center,
             style: TextStyle(
-              fontSize: 40,
+              fontSize: 50,
               fontWeight: FontWeight.w800,
               fontFamily: 'Aero',
               letterSpacing: 3.0,
@@ -85,13 +219,13 @@ class Menu extends StatelessWidget {
           ),
         ),
         Positioned(
-          left: 78,
-          top: 267,
+          left: 52,
+          bottom: 12,
           child: Text(
             'and match',
             textAlign: TextAlign.center,
             style: TextStyle(
-              fontSize: 40,
+              fontSize: 50,
               fontWeight: FontWeight.w800,
               fontFamily: 'Aero',
               color: Colors.white,
